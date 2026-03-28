0789ae6bf95dc0c5eb57b4562d7181f76f143307
k07g <korenaga.makoto@ea-sys.jp>
2026-03-28 15:18:07 +0900

    ECRライフサイクルポリシー追加

--- aws/sb/ecr.tf
+++ aws/sb/ecr.tf
@@ -6,3 +6,24 @@ resource "aws_ecr_repository" "g2" {
     scan_on_push = true
   }
 }
+
+data "aws_ecr_lifecycle_policy_document" "untagged" {
+  rule {
+    priority    = 1
+    description = "Expire untagged images older than 14 days"
+    selection {
+      tag_status   = "untagged"
+      count_type   = "sinceImagePushed"
+      count_number = 14
+      count_unit   = "days"
+    }
+    action {
+      type = "expire"
+    }
+  }
+}
+
+resource "aws_ecr_lifecycle_policy" "g2" {
+  repository = aws_ecr_repository.g2.name
+  policy     = data.aws_ecr_lifecycle_policy_document.untagged.json
+}
