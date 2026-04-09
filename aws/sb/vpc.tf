cabbe6742eb94b7b199295a4f8ed6a5c57d02069
k07g <155704074+k07g@users.noreply.github.com>
2026-04-09 10:09:00 +0900

    サブネットを追加、不要なインポートを削除 (#44)

--- aws/sb/vpc.tf
+++ aws/sb/vpc.tf
@@ -13,3 +13,11 @@ resource "aws_subnet" "g2" {
     Name = "g2"
   }
 }
+
+resource "aws_subnet" "planning_poker_public" {
+  vpc_id     = aws_vpc.g.id
+  cidr_block = "10.1.2.0/24"
+  tags = {
+    Name = "planning_poker_public"
+  }
+}
